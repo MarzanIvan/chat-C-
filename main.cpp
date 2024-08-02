533e1697a84c2995be119ccbd09ebe9495882e78
MarzanIvan <boss.marzan@bk.ru>
2024-08-02 16:00:58 +0300

    Try to connect to google.com by tcp socket

--- main.cpp
+++ main.cpp
@@ -2,8 +2,20 @@
 
 #define ASIO_STANDALONE
 #include <boost/asio.hpp>
+#include <asio/ts/internet.hpp>
+#include <asio/ts/buffer.hpp>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    asio::error_code ec;
+    asio::io_context context;
+    asio::ip::tcp::endpoint endpoint(asio::ip::make_address("173.194.221.113", ec), 80);
+    asio::ip::tcp::socket socket(context);
+
+    socket.connect(endpoint, ec);
+    if (!ec) {
+        std::cout << "Connected";
+    } else {
+        std::cout << "Something went wrong!";
+    }
     return 0;
 }
